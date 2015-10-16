2ca8f779d2b05d6a1e6c0727901365d6d9ca128f
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2015-10-16 05:15:30 +0330

    added tests.py

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+from auth import Authorization
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(1,1)
+
+if __name__ == '__main__':
+        unittest.main()
