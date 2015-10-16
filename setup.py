51ad3466380ddeffabb088c1f20d498626b0e640
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2015-10-16 17:37:26 +0330

    version 0.1.9

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ ext_modules = []
 
 setup(
     name='auth',
-    version='0.1.8',
+    version='0.1.9',
     description='Authorization for humans',
     long_description=io.open('README.rst', 'r', encoding='utf-8').read(),
     classifiers=[
