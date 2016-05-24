f676ac54dd11dbd5468c41d7d9f7d925a784acdf
Farsheed Ashouri <farsheed.ashouri@live.com>
2016-05-25 03:17:04 +0430

    updated to falcon 1.0.0

--- auth/CAS/REST/service.py
+++ auth/CAS/REST/service.py
@@ -15,6 +15,59 @@ except ImportError:
 from auth.CAS.authorization import Authorization
 
 
+class AuthComponent(object):
+    def process_request(self, req, resp):
+        """Process the request before routing it.
+
+        Args:
+            req: Request object that will eventually be
+                routed to an on_* responder method.
+            resp: Response object that will be routed to
+                the on_* responder.
+        """
+
+    def process_resource(self, req, resp, resource, params):
+        """Process the request after routing.
+
+        Note:
+            This method is only called when the request matches
+            a route to a resource.
+
+        Args:
+            req: Request object that will be passed to the
+                routed responder.
+            resp: Response object that will be passed to the
+                responder.
+            resource: Resource object to which the request was
+                routed.
+            params: A dict-like object representing any additional
+                params derived from the route's URI template fields,
+                that will be passed to the resource's responder
+                method as keyword arguments.
+        """
+
+    def process_response(self, req, resp, resource):
+        """Post-processing of the response (after routing).
+
+        Args:
+            req: Request object.
+            resp: Response object.
+            resource: Resource object to which the request was
+                routed. May be None if no route was found
+                for the request.
+        """
+        if isinstance(resp.body, dict):
+            try:
+                resp.body = json.dumps(resp.body)
+            except(nameError):
+                resp.status = falcon.HTTP_500
+
+
+
+
+
+
+
 def stringify(req, resp):
     """
     dumps all valid jsons
@@ -139,7 +192,7 @@ class Role:
 
 
 
-api = falcon.API(after=[stringify])
+api = falcon.API(middleware=[AuthComponent()])
 api.add_route('/ping', Ping())
 api.add_route('/api/membership/{client}/{user}/{group}', Membership())  ## POST DELETE GET
 api.add_route('/api/permission/{client}/{group}/{name}', Permission())  ## POST DELETE GET
